45f4ac07c2479c50497dbc6b0c80ed671ed452c1
Sarah Bill <sarahbill33@gmail.com>
2021-09-05 18:51:01 -0400

    title printed

---
+++ assignment1_part2.py
@@ -0,0 +1,15 @@
+class Book:
+    author = ""
+    title = ""
+    def __init__(self, author, title):
+        self.author = author
+        self.title = title
+    def display(self):
+        print(self.title)
+
+
+if __name__ == "__main__":
+    book1 = Book("John Steinbeck","Of Mice and Men")
+    book2 = Book("Harper Lee","To Kill a Mockingbird")
+    book1.display()
+    book2.display()
